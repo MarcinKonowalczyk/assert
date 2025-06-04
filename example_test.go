388294e68e42b309449027245de8e54b4dd7f252
Marcin <marcin.konow@lczyk.xyz>
2025-06-04 13:10:07 +0100

    add readme

---
+++ example_test.go
@@ -0,0 +1,31 @@
+package assert_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MarcinKonowalczyk/assert"
+)
+
+func ExampleThat() {
+
+	t := &testing.T{}
+	assert.That(t, true, "This should always pass")
+	fmt.Println(t.Failed())
+
+	assert.That(t, false, "This should always fail")
+	fmt.Println(t.Failed())
+
+	// output:
+	//
+	// false
+	// true
+}
+
+func TestExample(t *testing.T) {
+	a := 1
+	b := 2
+	if a == b {
+		t.Errorf("Expected %d to not equal %d", a, b)
+	}
+}
